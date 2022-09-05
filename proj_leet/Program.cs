69ceafa1a17a8045acd8b06cc454e8c5bfba779d
zhyq <1064639871@qq.com>
2022-09-05 16:43:22 +0800

    139. 单词拆分

--- proj_leet/Program.cs
+++ proj_leet/Program.cs
@@ -25,34 +25,50 @@ namespace proj_leet
         // }
 
         //完全背包问题测试
+        // static void Main(string[] args)
+        // {
+        //     // int N = 7, V = 20;
+        //     // int[] v = new int[] {4, 2, 3, 5, 1, 6, 7};
+        //     // int[] w = new int[] {4, 2, 3, 6, 2, 5, 9};
+
+        //     // Console.WriteLine("max: {0}", AllPack.maxValue(N, V, v, w));
+        //     // Console.WriteLine("dp1: {0}", AllPack.Dp1(N, v, w, 0, V));
+        //     // Console.WriteLine("dp2: {0}", AllPack.Dp2(N, v, w, V));
+            
+        //     // Console.WriteLine("dp2: {0}", Solution.Dp2(N, V, v, w));
+        //     // Console.WriteLine("dp3: {0}", Solution.Dp3(N, V, v, w));
+        //     // Console.WriteLine("dp4: {0}", Solution.Dp4(N, V, v, w));
+
+        //     // Console.WriteLine("dp0: {0}", Square279.NumSquares(13));
+
+        //     WorldSearch79 worldSearch79 = new WorldSearch79();
+        //     // char[][] board = new char[][]{new char[]{'A','B','C','E'},new char[]{'S','F','C','S'},new char[]{'A','D','E','E'}};
+        //     // string word = new string("ABCB");
+        //     // char[][] board = new char[][]{new char[]{'a'}};
+        //     // string word = "a";
+
+        //      char[][] board = new char[][]{new char[]{'a','b'},new char[]{'c','d'}};
+        //     string word = new string("cdba");
+        // //   char[][] board = new char[][]{new char[]{'a'},new char[]{'b'}};
+        // //     string word = new string("ba");
+        //     Console.WriteLine("exist0: {0}", worldSearch79.Exist(board, word));
+            
+            
+        // }
+
         static void Main(string[] args)
         {
-            // int N = 7, V = 20;
-            // int[] v = new int[] {4, 2, 3, 5, 1, 6, 7};
-            // int[] w = new int[] {4, 2, 3, 6, 2, 5, 9};
+            // var s = "leetcode";
+            // var wordDict = new string[]{"leet", "code"};
 
-            // Console.WriteLine("max: {0}", AllPack.maxValue(N, V, v, w));
-            // Console.WriteLine("dp1: {0}", AllPack.Dp1(N, v, w, 0, V));
-            // Console.WriteLine("dp2: {0}", AllPack.Dp2(N, v, w, V));
-            
-            // Console.WriteLine("dp2: {0}", Solution.Dp2(N, V, v, w));
-            // Console.WriteLine("dp3: {0}", Solution.Dp3(N, V, v, w));
-            // Console.WriteLine("dp4: {0}", Solution.Dp4(N, V, v, w));
+            // var s = "applepenapple";
+            // var wordDict = new string[]{"apple", "pen"};
 
-            // Console.WriteLine("dp0: {0}", Square279.NumSquares(13));
+            var s = "catsandog";
+            var wordDict = new string[]{"cats", "dog", "sand", "and", "cat"};
 
-            WorldSearch79 worldSearch79 = new WorldSearch79();
-            // char[][] board = new char[][]{new char[]{'A','B','C','E'},new char[]{'S','F','C','S'},new char[]{'A','D','E','E'}};
-            // string word = new string("ABCB");
-            // char[][] board = new char[][]{new char[]{'a'}};
-            // string word = "a";
-
-             char[][] board = new char[][]{new char[]{'a','b'},new char[]{'c','d'}};
-            string word = new string("cdba");
-        //   char[][] board = new char[][]{new char[]{'a'},new char[]{'b'}};
-        //     string word = new string("ba");
-            Console.WriteLine("exist0: {0}", worldSearch79.Exist(board, word));
-            
+            var instance = new WordBreak139();
+            Console.WriteLine("word break: {0}", instance.WordBreak(s, wordDict));
             
         }
     }
--- proj_leet/WordBreak139.cs
+++ proj_leet/WordBreak139.cs
@@ -1,1 +1,44 @@
+using System;
+using System.Collections.Generic;
 
+class WordBreak139 
+{
+    public bool WordBreak(string s, IList<string> wordDict) 
+    {
+        var dp = new Dictionary<string, bool>();
+        return process(s, wordDict, dp);
+    }
+
+    public bool process(string rest, IList<string> wordDict, Dictionary<string, bool> dp) 
+    {
+        if (dp.ContainsKey(rest))
+            return dp[rest];
+
+        if (rest.Length == 0) {
+            return true;
+        }
+
+        bool ans = false;
+        foreach (var word in wordDict)
+        {
+            if (rest.Length >= word.Length) {
+                if (rest.Substring(0, word.Length) == word) {
+                    ans = ans ||  process(rest.Substring(word.Length), wordDict, dp);
+                }
+            } 
+        }
+
+        dp[rest] = ans;
+
+        return ans;
+    }
+
+    // static void Main(string[] args)
+    // {
+    //     var s = "leetcode";
+    //     var wordDict = new string[]{"leet", "code"};
+    //     var instance = new WordBreak139();
+    //     Console.WriteLine("word break: {0}", instance.WordBreak(s, wordDict));
+        
+    // }
+}
