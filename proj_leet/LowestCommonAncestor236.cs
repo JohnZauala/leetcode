3e258063e8d4e8a7deca49dadde521602dcc2e1c
zhyq <1064639871@qq.com>
2022-09-09 10:13:43 +0800

    二叉树公共主线
    https://leetcode.cn/problems/lowest-common-ancestor-of-a-binary-tree/

---
+++ proj_leet/LowestCommonAncestor236.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+//https://leetcode.cn/problems/lowest-common-ancestor-of-a-binary-tree/
+class LowestCommonAncestor
+{
+    public TreeNode LowestCommonAncestorProcess(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) 
+            return root;
+        
+        var left = LowestCommonAncestorProcess(root.left, p, q);
+        var right = LowestCommonAncestorProcess(root.right, p, q);
+        if (right != null && left != null) {
+            return root;
+        }
+        return left != null ? left : right;
+    }
+
+    static void Main(string[] args)
+    {
+       
+        
+    }
+}
